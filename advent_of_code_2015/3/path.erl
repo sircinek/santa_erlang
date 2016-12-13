be28b9f71dc4004c329048c2f804f98a580e56fb
marcin <sircinek@gmail.com>
2016-12-13 23:26:24 +0100

    Added path module.

---
+++ advent_of_code_2015/3/path.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%%% @author marcin
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. Dec 2016 5:35 PM
+%%%-------------------------------------------------------------------
+-module(path).
+-author("marcin").
+
+%% API
+-export([track_movement/1]).
+track_movement(Input) -> track_movement(Input,[0,0],[]).
+track_movement([],_, Coordinates) -> Coordinates;
+track_movement([H|T],[X,Y], Coordinates) when H == $^ -> track_movement(T,[X,Y+1],[Coordinates,[X,Y+1]]);
+track_movement([H|T],[X,Y], Coordinates) when H == $< -> track_movement(T,[X-1,Y],[Coordinates,[X-1,Y]]);
+track_movement([H|T],[X,Y], Coordinates) when H == $v -> track_movement(T,[X,Y-1],[Coordinates,[X,Y+1]]);
+track_movement([H|T],[X,Y],Coordinates) when H == $> -> track_movement(T,[X+1,Y],[Coordinates,[X+1,Y]]).
