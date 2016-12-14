69558d77194a0bb8f2ea9f410bbd22d8d5041da1
sircinek <sircinek@gmail.com>
2016-12-14 10:04:05 +0100

    Refactored path module.

--- advent_of_code_2015/3/path.erl
+++ advent_of_code_2015/3/path.erl
@@ -10,33 +10,28 @@
 -author("marcin").
 
 %% API
--export([santa_count_gifts/1, robot_santa_count_gifts/1]).
+-export([count_gifts/2]).
 santa_track_movement(Input) -> santa_track_movement(Input,{0,0},[{0,0}]).
 santa_track_movement([],_, Coordinates) -> Coordinates;
-santa_track_movement([H|T],{X,Y}, Coordinates) when H == $^ -> santa_track_movement(T,{X,Y+1}, add_to_list({X,Y+1},Coordinates));
-santa_track_movement([H|T],{X,Y}, Coordinates) when H == $< -> santa_track_movement(T,{X-1,Y},add_to_list({X-1,Y},Coordinates));
-santa_track_movement([H|T],{X,Y}, Coordinates) when H == $v -> santa_track_movement(T,{X,Y-1},add_to_list({X,Y-1},Coordinates));
-santa_track_movement([H|T],{X,Y},Coordinates) when H == $> -> santa_track_movement(T,{X+1,Y},add_to_list({X+1,Y},Coordinates)).
+santa_track_movement([H|T],{X,Y}, Coordinates)  -> Change = move(H,{X,Y}),
+  santa_track_movement(T,Change, add_to_list(Change,Coordinates)).
 robot_santa_track_movement(Input) -> robot_santa_track_movement(Input,{0,0},{0,0},[{0,0}],santa).
 robot_santa_track_movement([],_,_,Coordinates,_) ->  Coordinates;
-robot_santa_track_movement([H|T],{X_santa,Y_santa}, Robot_Coords, Coordinates,santa) when H == $^ ->robot_santa_track_movement(T,{X_santa,Y_santa+1},Robot_Coords,add_to_list({X_santa,Y_santa+1},Coordinates),robot);
-robot_santa_track_movement([H|T],{X_santa,Y_santa}, Robot_Coords, Coordinates,santa) when H == $< ->robot_santa_track_movement(T,{X_santa-1,Y_santa},Robot_Coords,add_to_list({X_santa-1,Y_santa},Coordinates),robot);
-robot_santa_track_movement([H|T],{X_santa,Y_santa}, Robot_Coords, Coordinates,santa) when H == $v ->robot_santa_track_movement(T,{X_santa,Y_santa-1},Robot_Coords,add_to_list({X_santa,Y_santa-1},Coordinates),robot);
-robot_santa_track_movement([H|T],{X_santa,Y_santa}, Robot_Coords, Coordinates,santa) when H == $> ->robot_santa_track_movement(T,{X_santa+1,Y_santa},Robot_Coords,add_to_list({X_santa+1,Y_santa},Coordinates),robot);
-robot_santa_track_movement([H|T],Santa_Coords, {X_robot,Y_robot}, Coordinates,robot) when H == $^ ->robot_santa_track_movement(T,Santa_Coords,{X_robot,Y_robot+1},add_to_list({X_robot,Y_robot+1},Coordinates),santa);
-robot_santa_track_movement([H|T],Santa_Coords, {X_robot,Y_robot}, Coordinates,robot) when H == $< ->robot_santa_track_movement(T,Santa_Coords,{X_robot-1,Y_robot},add_to_list({X_robot-1,Y_robot},Coordinates),santa);
-robot_santa_track_movement([H|T],Santa_Coords, {X_robot,Y_robot}, Coordinates,robot) when H == $v ->robot_santa_track_movement(T,Santa_Coords,{X_robot,Y_robot-1},add_to_list({X_robot,Y_robot-1},Coordinates),santa);
-robot_santa_track_movement([H|T],Santa_Coords, {X_robot,Y_robot}, Coordinates,robot) when H == $> ->robot_santa_track_movement(T,Santa_Coords,{X_robot+1,Y_robot},add_to_list({X_robot+1,Y_robot},Coordinates),santa).
-
-
-
-
-
+robot_santa_track_movement([H|T],{X,Y},Robot_cords,Coordinates,santa) -> Change = move(H,{X,Y}),
+  robot_santa_track_movement(T,Change,Robot_cords,add_to_list(Change,Coordinates),robot);
+robot_santa_track_movement([H|T],Santa_cords,{X,Y},Coordinates,robot) -> Change = move(H,{X,Y}),
+  robot_santa_track_movement(T,Santa_cords,Change,add_to_list(Change,Coordinates),santa).
+move (A,{X,Y}) when A == $^ -> {X,Y+1};
+move (A,{X,Y}) when A == $v -> {X,Y-1};
+move (A,{X,Y}) when A == $> -> {X+1,Y};
+move (A,{X,Y}) when A == $< -> {X-1,Y}.
 
 add_to_list(New,Current) ->
   case lists:member(New,Current) of
     true -> Current;
     false -> [New|Current]
   end.
-santa_count_gifts(Input) -> length(santa_track_movement((Input))).
-robot_santa_count_gifts(Input)-> length(robot_santa_track_movement(Input)).
+count_gifts(Input,santa) -> length(santa_track_movement((Input)));
+count_gifts(Input,robot) -> length(robot_santa_track_movement((Input)));
+count_gifts([],_) -> io:format("Not a valid list to compute !~n");
+count_gifts(_,_) -> io:format("Not a valid gift deliverer my friend !~n").
