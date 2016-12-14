e3c6edd1487b9f3371961049ecaa2c7e3b34ef7f
sircinek <sircinek@gmail.com>
2016-12-14 07:40:22 +0100

    Updated path module.

--- advent_of_code_2015/3/path.erl
+++ advent_of_code_2015/3/path.erl
@@ -11,9 +11,9 @@
 
 %% API
 -export([track_movement/1]).
-track_movement(Input) -> track_movement(Input,[0,0],[]).
+track_movement(Input) -> track_movement(Input,[{0,0}],[{0,0}]).
 track_movement([],_, Coordinates) -> Coordinates;
-track_movement([H|T],[X,Y], Coordinates) when H == $^ -> track_movement(T,[X,Y+1],[Coordinates,[X,Y+1]]);
-track_movement([H|T],[X,Y], Coordinates) when H == $< -> track_movement(T,[X-1,Y],[Coordinates,[X-1,Y]]);
-track_movement([H|T],[X,Y], Coordinates) when H == $v -> track_movement(T,[X,Y-1],[Coordinates,[X,Y+1]]);
-track_movement([H|T],[X,Y],Coordinates) when H == $> -> track_movement(T,[X+1,Y],[Coordinates,[X+1,Y]]).
+track_movement([H|T],[{X,Y}], Coordinates) when H == $^ -> track_movement(T,[{X,Y+1}],lists:append(Coordinates,[{X,Y+1}]));
+track_movement([H|T],[{X,Y}], Coordinates) when H == $< -> track_movement(T,[{X-1,Y}],lists:append(Coordinates,[{X-1,Y}]));
+track_movement([H|T],[{X,Y}], Coordinates) when H == $v -> track_movement(T,[{X,Y-1}],lists:append(Coordinates,[{X,Y-1}]));
+track_movement([H|T],[{X,Y}],Coordinates) when H == $> -> track_movement(T,[{X+1,Y}],lists:append(Coordinates,[{X+1,Y}])).
