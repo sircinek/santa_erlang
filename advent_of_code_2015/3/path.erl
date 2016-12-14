2a660d01161d4b780bf59f7bc60aab96f8d6d058
sircinek <sircinek@gmail.com>
2016-12-14 09:27:59 +0100

    Initial draft. To be refactored ! Some functions needs to be commonized. This gives correct answer only.

--- advent_of_code_2015/3/path.erl
+++ advent_of_code_2015/3/path.erl
@@ -10,16 +10,33 @@
 -author("marcin").
 
 %% API
--export([count_gifts/1]).
-track_movement(Input) -> track_movement(Input,{0,0},[{0,0}]).
-track_movement([],_, Coordinates) -> Coordinates;
-track_movement([H|T],{X,Y}, Coordinates) when H == $^ -> track_movement(T,{X,Y+1}, add_to_list({X,Y+1},Coordinates));
-track_movement([H|T],{X,Y}, Coordinates) when H == $< -> track_movement(T,{X-1,Y},add_to_list({X-1,Y},Coordinates));
-track_movement([H|T],{X,Y}, Coordinates) when H == $v -> track_movement(T,{X,Y-1},add_to_list({X,Y-1},Coordinates));
-track_movement([H|T],{X,Y},Coordinates) when H == $> -> track_movement(T,{X+1,Y},add_to_list({X+1,Y},Coordinates)).
+-export([santa_count_gifts/1, robot_santa_count_gifts/1]).
+santa_track_movement(Input) -> santa_track_movement(Input,{0,0},[{0,0}]).
+santa_track_movement([],_, Coordinates) -> Coordinates;
+santa_track_movement([H|T],{X,Y}, Coordinates) when H == $^ -> santa_track_movement(T,{X,Y+1}, add_to_list({X,Y+1},Coordinates));
+santa_track_movement([H|T],{X,Y}, Coordinates) when H == $< -> santa_track_movement(T,{X-1,Y},add_to_list({X-1,Y},Coordinates));
+santa_track_movement([H|T],{X,Y}, Coordinates) when H == $v -> santa_track_movement(T,{X,Y-1},add_to_list({X,Y-1},Coordinates));
+santa_track_movement([H|T],{X,Y},Coordinates) when H == $> -> santa_track_movement(T,{X+1,Y},add_to_list({X+1,Y},Coordinates)).
+robot_santa_track_movement(Input) -> robot_santa_track_movement(Input,{0,0},{0,0},[{0,0}],santa).
+robot_santa_track_movement([],_,_,Coordinates,_) ->  Coordinates;
+robot_santa_track_movement([H|T],{X_santa,Y_santa}, Robot_Coords, Coordinates,santa) when H == $^ ->robot_santa_track_movement(T,{X_santa,Y_santa+1},Robot_Coords,add_to_list({X_santa,Y_santa+1},Coordinates),robot);
+robot_santa_track_movement([H|T],{X_santa,Y_santa}, Robot_Coords, Coordinates,santa) when H == $< ->robot_santa_track_movement(T,{X_santa-1,Y_santa},Robot_Coords,add_to_list({X_santa-1,Y_santa},Coordinates),robot);
+robot_santa_track_movement([H|T],{X_santa,Y_santa}, Robot_Coords, Coordinates,santa) when H == $v ->robot_santa_track_movement(T,{X_santa,Y_santa-1},Robot_Coords,add_to_list({X_santa,Y_santa-1},Coordinates),robot);
+robot_santa_track_movement([H|T],{X_santa,Y_santa}, Robot_Coords, Coordinates,santa) when H == $> ->robot_santa_track_movement(T,{X_santa+1,Y_santa},Robot_Coords,add_to_list({X_santa+1,Y_santa},Coordinates),robot);
+robot_santa_track_movement([H|T],Santa_Coords, {X_robot,Y_robot}, Coordinates,robot) when H == $^ ->robot_santa_track_movement(T,Santa_Coords,{X_robot,Y_robot+1},add_to_list({X_robot,Y_robot+1},Coordinates),santa);
+robot_santa_track_movement([H|T],Santa_Coords, {X_robot,Y_robot}, Coordinates,robot) when H == $< ->robot_santa_track_movement(T,Santa_Coords,{X_robot-1,Y_robot},add_to_list({X_robot-1,Y_robot},Coordinates),santa);
+robot_santa_track_movement([H|T],Santa_Coords, {X_robot,Y_robot}, Coordinates,robot) when H == $v ->robot_santa_track_movement(T,Santa_Coords,{X_robot,Y_robot-1},add_to_list({X_robot,Y_robot-1},Coordinates),santa);
+robot_santa_track_movement([H|T],Santa_Coords, {X_robot,Y_robot}, Coordinates,robot) when H == $> ->robot_santa_track_movement(T,Santa_Coords,{X_robot+1,Y_robot},add_to_list({X_robot+1,Y_robot},Coordinates),santa).
+
+
+
+
+
+
 add_to_list(New,Current) ->
   case lists:member(New,Current) of
     true -> Current;
     false -> [New|Current]
   end.
-count_gifts(Input) -> length(track_movement((Input))).
+santa_count_gifts(Input) -> length(santa_track_movement((Input))).
+robot_santa_count_gifts(Input)-> length(robot_santa_track_movement(Input)).
