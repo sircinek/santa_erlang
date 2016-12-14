95a8084e0eda52c1e26cd5668a3c248742eb16f0
sircinek <sircinek@gmail.com>
2016-12-14 08:18:38 +0100

    Updated path module.

--- advent_of_code_2015/3/path.erl
+++ advent_of_code_2015/3/path.erl
@@ -10,10 +10,16 @@
 -author("marcin").
 
 %% API
--export([track_movement/1]).
-track_movement(Input) -> track_movement(Input,[{0,0}],[{0,0}]).
+-export([track_movement/1,count_gifts/1]).
+track_movement(Input) -> track_movement(Input,{0,0},[{0,0}]).
 track_movement([],_, Coordinates) -> Coordinates;
-track_movement([H|T],[{X,Y}], Coordinates) when H == $^ -> track_movement(T,[{X,Y+1}],lists:append(Coordinates,[{X,Y+1}]));
-track_movement([H|T],[{X,Y}], Coordinates) when H == $< -> track_movement(T,[{X-1,Y}],lists:append(Coordinates,[{X-1,Y}]));
-track_movement([H|T],[{X,Y}], Coordinates) when H == $v -> track_movement(T,[{X,Y-1}],lists:append(Coordinates,[{X,Y-1}]));
-track_movement([H|T],[{X,Y}],Coordinates) when H == $> -> track_movement(T,[{X+1,Y}],lists:append(Coordinates,[{X+1,Y}])).
+track_movement([H|T],{X,Y}, Coordinates) when H == $^ -> track_movement(T,{X,Y+1}, add_to_list({X,Y+1},Coordinates));
+track_movement([H|T],{X,Y}, Coordinates) when H == $< -> track_movement(T,{X-1,Y},add_to_list({X-1,Y},Coordinates));
+track_movement([H|T],{X,Y}, Coordinates) when H == $v -> track_movement(T,{X,Y-1},add_to_list({X,Y-1},Coordinates));
+track_movement([H|T],{X,Y},Coordinates) when H == $> -> track_movement(T,{X+1,Y},add_to_list({X+1,Y},Coordinates)).
+add_to_list(New,Current) ->
+  case lists:member(New,Current) of
+    true -> Current;
+    false -> [New|Current]
+  end.
+count_gifts(Houses) -> lists:sum(Houses).
